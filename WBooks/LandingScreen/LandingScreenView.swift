096c7414cdab5ea91c33a2917b76e2994ea01612
micaelacole <micaela.cole@wolox.com.ar>
2020-01-13 15:39:48 -0300

    Landing screen created.

---
+++ WBooks/LandingScreen/LandingScreenView.swift
@@ -0,0 +1,13 @@
+//
+//  LandingScreenView.swift
+//  WBooks
+//
+//  Created by Megan Cole on 08/01/2020.
+//  Copyright © 2020 Wolox. All rights reserved.
+//
+
+import SwiftUI
+import WolmoCore
+
+class LandingScreenView: UIView, NibLoadable {
+}
