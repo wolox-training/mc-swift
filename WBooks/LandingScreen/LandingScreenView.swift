04efa519bc6aa919d0e3cf7fa7935dec012d840a
micaelacole <micaela.cole@wolox.com.ar>
2020-01-14 02:15:06 -0300

    Login button working as expected.

--- WBooks/LandingScreen/LandingScreenView.swift
+++ WBooks/LandingScreen/LandingScreenView.swift
@@ -10,4 +10,15 @@ import SwiftUI
 import WolmoCore
 
 class LandingScreenView: UIView, NibLoadable {
+    @IBAction func loginWithGoogle(_ sender: UIButton) {
+        print("LOGIN WITH GOOGLE")
+    }
+
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            loginButton.layer.cornerRadius = 25
+            loginButton.layer.borderWidth = 2
+            loginButton.layer.borderColor = UIColor.white.cgColor
+        }
+    }
 }
