226f8ed2bbaa23a50e3f2dddf6921b8292a9a3ce
micaelacole <micaela.cole@wolox.com.ar>
2020-02-21 16:56:03 -0300

    Replaced fixed label strings with the localized ones.

--- WBooks/LandingScreen/LandingScreenView.swift
+++ WBooks/LandingScreen/LandingScreenView.swift
@@ -14,6 +14,15 @@ class LandingScreenView: UIView, NibLoadable {
             loginButton.layer.cornerRadius = 25
             loginButton.layer.borderWidth = 2
             loginButton.layer.borderColor = UIColor.white.cgColor
+            loginButton.setTitle("LOGIN_BUTTON".localized(), for: UIControl.State.normal)
+            loginButton.setTitleColor(UIColor.white, for: .normal)
+            loginButton.titleLabel?.font = UIFont(name: "TrebuchetMS-Bold", size: 18)
+            }
+    }
+    @IBOutlet weak var footerLabel: UILabel! {
+        didSet {
+            footerLabel.text = "FOOTER_LABEL".localized()
+            footerLabel.font = UIFont.systemFont(ofSize: 12)
         }
     }
 }
