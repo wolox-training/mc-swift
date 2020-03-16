35430bb0ff699b356c043675cfdd45609f20b79c
micaelacole <micaela.cole@wolox.com.ar>
2020-03-16 03:03:03 -0300

    Images are getting errors because of insecure url session.

--- WBooks/Tabs/Library/LibraryController.swift
+++ WBooks/Tabs/Library/LibraryController.swift
@@ -69,7 +69,14 @@ extension LibraryController: UITableViewDataSource {
         if let url = URL(string: book.image) {
             let resource = ImageResource(downloadURL: url)
             cell.bookCover.kf.indicatorType = .activity
-            cell.bookCover.kf.setImage(with: resource)
+            cell.bookCover.kf.setImage(with: resource, placeholder: UIImage.book5) { result in
+                switch result {
+                case .success(let value):
+                    print("SUCCESS: \(value.image).")
+                case .failure (let error):
+                    print("ERROR: \(error).")
+                }
+            }
         } else {
             cell.bookCover.image = UIImage.book1
         }
